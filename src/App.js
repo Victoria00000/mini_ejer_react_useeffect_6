2dad575c4f33c1464b6f7167cb1fc3f6153876ee
Victoria00000 <vvictoriaandres@gmail.com>
2022-02-16 13:31:37 -0300

    1er commit, iniciando un fetch con useEffect + useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const ApiUrl = 'https://randomuser.me/api/';
 
 function App() {
+  const [names, setNames] = useState('');
+  const [src, setSrc] = useState('');
+  useEffect(() => {
+    const Fetch = fetch(ApiUrl);
+    Fetch
+      .then(data => data.json())
+      .then(datos => {
+        console.log(datos.results);
+        return (
+          setNames(`${datos.results[0].name.first} ${datos.results[0].name.last}`),
+          setSrc(`${datos.results[0].picture.large}`))
+      }).catch(() => console.log('error'));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{names}</p>
+      <div><img src={src} alt='img' /></div>
     </div>
   );
-}
+};
 
 export default App;
